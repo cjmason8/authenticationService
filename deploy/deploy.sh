a848ab6c1f4179800ae5a0fe3fca3e394ba88009
Chris Mason <cjmason8@gmail.com>
2016-11-21 21:21:41 +1100

    fix typo

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -22,5 +22,5 @@ docker login --username=cjmason8 --password=$PASSWORD
 echo "docker pull"
 docker pull cjmason8/auth-service:latest
 echo "docker compose"
-docker-compose -f ${ENV}/docker-compose.${ENV}.yml up -d authService
+docker-compose -f ${ENV}/docker-compose-${ENV}.yml up -d authService
 echo "finished"
