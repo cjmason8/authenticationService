c46c0aa6d8bc4dc1c23536a107b5896e21fc3d2b
Chris Mason <cjmason8@gmail.com>
2017-03-15 21:07:45 +1100

    add global CORS

--- src/main/java/au/com/mason/authservice/controller/AuthenticateController.java
+++ src/main/java/au/com/mason/authservice/controller/AuthenticateController.java
@@ -1,7 +1,6 @@
 package au.com.mason.authservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +14,6 @@ public class AuthenticateController {
 	@Autowired
 	private SessionTokenService sessionTokenService;
 
-	@CrossOrigin(origins = { "http://localhost:4200", "http://80.241.221.122:8083" })
 	@RequestMapping(value = "/authenticate/{token}", method = RequestMethod.GET, produces = "application/json")
     String login(@PathVariable String token) {
 		if (sessionTokenService.validateToken(token)) {
