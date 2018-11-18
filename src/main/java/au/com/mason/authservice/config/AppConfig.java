bb9a7e658d87f229b2ef3e2e83f041829f3dd454
Chris Mason <cjmason8@gmail.com>
2018-11-18 20:34:23 +1100

    change cors~

--- src/main/java/au/com/mason/authservice/config/AppConfig.java
+++ src/main/java/au/com/mason/authservice/config/AppConfig.java
@@ -1,21 +1,14 @@
 package au.com.mason.authservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("PUT", "DELETE", "GET", "POST");
-			}
-		};
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("PUT", "DELETE", "GET", "POST");
 	}
+
 }
